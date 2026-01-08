5fcee08d7e6504d5d10b30b15e8089cdbdd22062
Javier Sanchez <javsan77@gmail.com>
2026-01-08 17:07:38 -0500

    Handler Ok

--- internal/handlers/task_handler.go
+++ internal/handlers/task_handler.go
@@ -33,6 +33,100 @@ func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
 		respondError(w, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	task := &models.Task{
+		Title:			req.Title,
+		Description: 	req.Description,
+		Completed: 		false,
+	}
+
+	if err := h.store.Create(task); err != nil {
+		respondError(w, http.StatusInternalServerError, "Failed to create task")
+		return
+	}
+
+	respondJSON(w, http.StatusCreated, task)
+}
+
+// GetTask manages GET /tasks/{id}
+func (h *TaskHandler) GetTask(w http.ResponseWriter, r *http.Request){
+	id, err := extractID(r.URL.Path)
+	if err != nil {
+		respondError(w, http.StatusBadRequest, "Invalid task ID")
+		return
+	}
+
+	task, err := h.store.GetByID(id)
+	if err != nil{
+		if err == models.ErrTaskNotFound{
+			respondError(w, http.StatusNotFound, "Task not found")
+			return
+		}
+		respondError(w, http.StatusInternalServerError, "Failed to get task")
+		return
+	}
+
+	respondJSON(w, http.StatusOK, task)
+}
+
+// GetAllTasks maneja GET /tasks
+func (h *TaskHandler) GetAllTasks(w http.ResponseWriter, r *http.Request){
+	tasks, err := h.store.GetAll()
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, "Failed to get tasks")
+		return
+	}
+
+	respondJSON(w, http.StatusOK, tasks)
+}
+
+// UpdateTask manages PUT /tasks/{id}
+func (h *TaskHandler) Update(w http.ResponseWriter, r *http.Request){
+	id, err := extractID(r.URL.Path)
+	if err != nil {
+		respondError(w, http.StatusBadRequest, "Invalid task ID")
+		return
+	}
+
+	var req models.UpdateTaskRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		respondError(w, http.StatusBadRequest, "Invalid request Body")
+		return
+	}
+
+	task, err := h.store.Update(id, &req)
+	if err != nil {
+		if err == models.ErrTaskNotFound {
+			respondError(w, http.StatusNotFound, "Task not found")
+			return
+		}
+		respondError(w, http.StatusInternalServerError, "Failed to update task")
+		return
+	}
+	
+	respondJSON(w, http.StatusOK, task)
+}
+
+
+// DeleteTask manages DELETE /tasks/{id}
+func (h *TaskHandler) DeleteTask(w http.ResponseWriter, r *http.Request) {
+	id, err := extractID(r.URL.Path)
+	if err != nil {
+		respondError(w, http.StatusBadRequest, "Invalid task ID")
+		return
+	}
+
+	err = h.store.Delete(id)
+	if err != nil {
+		if err == models.ErrTaskNotFound {
+			respondError(w, http.StatusNotFound, "Task not found")
+			return
+		}
+		respondError(w, http.StatusInternalServerError, "Failed to delete task")
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // Auxiliary Functions
